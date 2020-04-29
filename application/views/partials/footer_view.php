0805c40f56a1b6d67ec321a6d359cb7ac9d611a5
dat <amidateri@gmail.com>
2020-04-29 18:22:37 +0700

    dat

---
+++ application/views/partials/footer_view.php
@@ -0,0 +1,35 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+    <section class="container-fluid footer-top2">
+        <section class="social-ico-bar">
+            <section class="container">
+                <section class="row-fluid">
+
+                </section>
+            </section>
+        </section>
+        <section class="container">
+            <section class="row-fluid">
+                <p><strong>Địa chỉ gửi hàng đổi/trả/bảo hành:</strong> Cửa hàng Thời Trang UniShop - 32, Cầu Giấy, Quận Cầu Giấy, Hà Nội  (Tham khảo <a href="" target="_blank">hướng dẫn đổi, trả, bảo hành</a> hoặc liên hệ 1900-8888 để được hướng dẫn trước khi gửi sản phẩm về Cửa hàng)</p>
+                        <p>Địa chỉ văn phòng: 36, Xuân Thủy, Phường Quan Hoa, Quận Cầu Giấy, Hà Nội</p>
+                        <p>Giấy chứng nhận Đăng ký Kinh doanh số 0309532909 do Sở Kế hoạch và Đầu tư Thành phố Hà Nội cấp ngày 06/01/2017</p>
+            </section>
+        </section>
+    </section>
+    <footer id="main-footer">
+        <section class="social-ico-bar">
+            <section class="container">
+                <section class="row-fluid">
+                    <article class="span6">
+                        <p>© 201 - Bản quyền của developer [Anh - Chung]</p>
+                    </article>
+                    <article class="span6 copy-right">
+                        <p>Cửa hàng <a href="?cn=home">Thời Trang UniShop</a></p>
+                    </article>
+                </section>
+            </section>
+        </section>
+    </footer>
+</div>
+</body>
+<?php ob_end_flush(); ?>
+</html>
